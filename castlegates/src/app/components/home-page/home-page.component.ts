92923f6d4d4af330421669ba7be7f3d2ae5d50b8
Caio <caio.dcc.marques@gmail.com>
2023-05-29 17:27:57 -0300

    29/05 2

--- castlegates/src/app/components/home-page/home-page.component.ts
+++ castlegates/src/app/components/home-page/home-page.component.ts
@@ -7,28 +7,11 @@ import  {faLinkedin, faGithub, faWhatsapp} from '@fortawesome/free-brands-svg-ic
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
-  dataTable: any[] = [];
+
   iconGit = faGithub;
   iconLink = faLinkedin;
   iconWp = faWhatsapp;
 
 
-  constructor(){
-  this.dataTable = [
-    { text: 'Lorem ipsum', date: '2023-05-25', tag: 'Tag1' },
-    { text: 'Dolor sit amet', date: '2023-05-24', tag: 'Tag2' },
-    { text: 'Consectetur adipiscing elit', date: '2023-05-23', tag: 'Tag3' },
-    { text: 'Dolor sit amet', date: '2023-05-24', tag: 'Tag2' },
-    { text: 'Dolor sit amet', date: '2023-05-24', tag: 'Tag2' },
-    { text: 'Dolor sit amet', date: '2023-05-24', tag: 'Tag2' },
-    { text: 'Dolor sit amet', date: '2023-05-24', tag: 'Tag2' },
-    { text: 'Dolor sit amet', date: '2023-05-24', tag: 'Tag2' },
-    { text: 'Dolor sit amet', date: '2023-05-24', tag: 'Tag2' },
-    { text: 'Dolor sit amet', date: '2023-05-24', tag: 'Tag2' },
-    { text: 'Dolor sit amet', date: '2023-05-24', tag: 'Tag2' },
-    { text: 'Dolor sit amet', date: '2023-05-24', tag: 'Tag2' },
-    { text: 'Dolor sit amet', date: '2023-05-24', tag: 'Tag2' },
-    { text: 'Dolor sit amet', date: '2023-05-24', tag: 'Tag2' },
-  ];
-}
+  
 }
---
+++ castlegates/src/app/components/text-read/text-read.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-read',
+  templateUrl: './text-read.component.html',
+  styleUrls: ['./text-read.component.css']
+})
+export class TextReadComponent {
+
+  constructor(){
+
+
+}
+}
