9d9f67fad120b554f2035885637100ce611017a6
Caio <caio.dcc.marques@gmail.com>
2023-05-31 13:32:14 -0300

    31/05

--- castlegates/src/app/components/projects/projects.component.ts
+++ castlegates/src/app/components/projects/projects.component.ts
@@ -8,10 +8,10 @@ import  { faGithub} from '@fortawesome/free-brands-svg-icons'
 export class ProjectsComponent {
   iconGit = faGithub;
   projects = [
-    { nome: 'Projeto 1', data: new Date(), descricao: 'Descrição do Projeto 1', img: 'assets/2.png', tag: 'Tecnologia'  },
-    { nome: 'Projeto 2', data: new Date(), descricao: 'Descrição do Projeto 2', img: 'assets/eu.jpg', tag: 'Tecnologia' },
-    { nome: 'Projeto 3', data: new Date(), descricao: 'Descrição do Projeto 3', img: 'assets/augusto-dos-anjos.jpg', tag: 'Tecnologia' },
-    { nome: 'Projeto 3', data: new Date(), descricao: 'Descrição do Projeto 3', img: 'assets/augusto-dos-anjos.jpg', tag: 'Tecnologia' },
+    { nome: 'Calculadora Flexível em Typescript', data: new Date(), descricao: 'Descrição do Projeto 1', img: 'assets/2.png', tag: 'Tecnologia'  },
+    { nome: 'CRUD Simples no Angular', data: new Date(), descricao: 'Descrição do Projeto 2', img: 'assets/eu.jpg', tag: 'Tecnologia' },
+    { nome: 'Este website!', data: new Date(), descricao: 'Descrição do Projeto 3', img: 'assets/augusto-dos-anjos.jpg', tag: 'Tecnologia' },
+    { nome: 'CRUD Simples no Java ', data: new Date(), descricao: 'Descrição do Projeto 3', img: 'assets/augusto-dos-anjos.jpg', tag: 'Tecnologia' },
   ]// ... mais projetos
 
 }
