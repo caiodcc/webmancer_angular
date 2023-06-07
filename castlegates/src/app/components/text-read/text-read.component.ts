1ff8e2085aefbb134127ba7b9924e6f1a71d72ee
Caio <caio.dcc.marques@gmail.com>
2023-06-07 11:59:26 -0300

    07/06

--- castlegates/src/app/components/text-read/text-read.component.ts
+++ castlegates/src/app/components/text-read/text-read.component.ts
@@ -16,9 +16,12 @@ export class TextReadComponent {
       const textId = params.get('id');
       // Use o ID do texto para carregar os dados necessários para exibir o texto
       this.text = {
-        title: 'Título do Texto',
-        date: '31 de Maio de 2023',
-        content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat leo vel arcu volutpat iaculis. Donec rutrum tincidunt purus, id commodo odio tincidunt vitae. Aliquam eleifend bibendum tempor. Morbi tempor, nisl eu ultricies commodo, dui tortor auctor nulla, id tristique dolor nunc ac enim.'
+        id: this.text.id,
+        tag: this.text.tag,
+        titulo: this.text.titulo,
+        subtitulo: this.text.subtitulo,
+        data: this.text.data,
+        conteudo: this.text.conteudo
       };
     });
   }
