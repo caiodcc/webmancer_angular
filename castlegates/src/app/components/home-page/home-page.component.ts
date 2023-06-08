7ac9a858ec7b69ffc14416d52d77a8fb8484c19a
Caio <caio.dcc.marques@gmail.com>
2023-06-08 14:58:30 -0300

    08/06

--- castlegates/src/app/components/home-page/home-page.component.ts
+++ castlegates/src/app/components/home-page/home-page.component.ts
@@ -29,20 +29,19 @@ export class HomePageComponent {
   // Default 
   defaultTituloResumo:string = `Quem sou eu?`
   ;
-  defaultResumo: string = `My name is Caio Marques, a 21-year-old web developer from Rio de Janeiro, Brazil, and this website is the
-    result of about 14 months of study. During these months, i managed to superficially explore a lot of content
-    related to development. I also managed to map out the road i have ahead, and i learned that skill alone is not
-    enough. I integrated myself into the tech community and have found that many problems come from lack of
-    cooperation and communication, ironically, the main reason any large-scale progress is made.
-    I intend to be different and exemplary for those who have or have had problems with how dynamic the tech
-    market can be.
-    My values are respect, objectivity, communication, cooperation, and
-    inclusion.
-    Artistically, i write poems and texts because i am a developer by nature, and i also like to develop
-    philosophies.
-    I am not afraid of putting my ego aside to ask for help or making mistakes, but i am not reckless too,
-    always seeking to recognize and expand my limits through self-education.` 
-  defaultDestaque:string = `Textos em Destaque`; 
+  defaultResumo: string = `Meu nome é Caio Marques, um desenvolvedor web de 21 anos do Rio de Janeiro, Brasil, e este site é o
+  resultado de cerca de 14 meses de estudo. Nesses meses, consegui explorar superficialmente muito conteúdo
+  relacionados ao desenvolvimento. Também consegui traçar o caminho que tenho pela frente e aprendi que habilidade sozinha não é
+  suficiente. Eu me integrei à comunidade de tecnologia e descobri que muitos problemas vêm da falta de
+  cooperação e comunicação, ironicamente, são a principal razão pela qual qualquer progresso em larga escala é feito.
+  Pretendo ser diferente e exemplar para quem tem ou já teve problemas com a dinâmica da tecnologia
+  mercado pode ser.
+  Meus valores são respeito, objetividade, comunicação, cooperação e
+  inclusão.
+  Artisticamente, escrevo poemas e textos porque sou um desenvolvedor por natureza, e também gosto de desenvolver
+  filosofias.
+  Não tenho medo de deixar meu ego de lado para pedir ajuda ou cometer erros, mas também não sou imprudente,
+  sempre buscando reconhecer e expandir meus limites através da autoeducação e autodidatismo.` 
   defaultTitulo: string = `Bem-vindx ao `; 
   defaultSubtituloUm:string = `Tecnologia e Arte`;
   defaultSubtituloDois:string = `Artigos, poesia, guias e reviews`;
@@ -59,7 +58,7 @@ export class HomePageComponent {
     this.tituloResumo = this.defaultTituloResumo
     ;
     this.resumo = this.defaultResumo;
-    this.destaque = this.defaultDestaque; 
+ 
     this.titulo = this.defaultTitulo; 
     this.subtituloUm = this.defaultSubtituloUm;
     this.subtituloDois = this.defaultSubtituloDois;
@@ -110,7 +109,7 @@ export class HomePageComponent {
         Artisticamente, escrevo poemas e textos porque sou um desenvolvedor por natureza, e também gosto de desenvolver
         filosofias.
         Não tenho medo de deixar meu ego de lado para pedir ajuda ou cometer erros, mas também não sou imprudente,
-        sempre buscando reconhecer e expandir meus limites através da autoeducação.`;
+        sempre buscando reconhecer e expandir meus limites através da autoeducação e autodidatismo.`;
         this.destaque = `Textos em Destaque`; 
         this.titulo = `Bem-vindx ao `; 
         this.subtituloUm = `Tecnologia e Arte`;
--- castlegates/src/app/components/projects/projects.component.ts
+++ castlegates/src/app/components/projects/projects.component.ts
@@ -8,10 +8,10 @@ import  { faGithub} from '@fortawesome/free-brands-svg-icons'
 export class ProjectsComponent {
   iconGit = faGithub;
   projects = [
-    { nome: 'Typescript Macronutrients Calculator', descricao: 'Typescript calculator to calculate the required amount of macronutrients for hypertrophy and other such goals.', img: 'assets/ts.png', tag: 'Technology', link: 'https://github.com/caiodcc/calculadora-dieta-flexivel'  },
-    { nome: 'Angular CRUD', descricao: 'First CRUD in Angular, general study of the framework, directives, templates and HTTP communication', img: 'assets/ng.png', tag: 'Technology', link: 'https://github.com/caiodcc/crud_Angular' },
-    { nome: 'This website!', descricao: 'Personal project for publishing own studies, contents and portfolio', img: 'assets/ng.png', tag: 'Technology', link: 'https://github.com/caiodcc/frontend_webmancer_angular'  },
-    { nome: 'NestJS CRUD ', descricao: 'Backend study for personal project and portfolio', img: 'assets/nst.png', tag: 'Technology', link: 'https://github.com/caiodcc/backend_webmancer_nestjs' },
+    { nome: 'Typescript Macronutrients Calculator', descricao: 'Typescript calculator to calculate the required amount of macronutrients for hypertrophy and other such goals.', img: 'assets/ts.png', tag: 'Frontend, Typescript, Angular, Bootstrap, Programming Logics', link: 'https://github.com/caiodcc/calculadora-dieta-flexivel'  },
+    { nome: 'Angular CRUD', descricao: 'First CRUD in Angular, general study of the framework, directives, templates and HTTP communication', img: 'assets/ng.png', tag: 'Frontend Angular HTTP, Typescript, Bootstrap, Directives', link: 'https://github.com/caiodcc/crud_Angular' },
+    { nome: 'This website!', descricao: 'Personal project for publishing own studies, contents and portfolio', img: 'assets/ng.png', tag: 'Angular, Typescript, Frontend, Bootstrap, Directives', link: 'https://github.com/caiodcc/frontend_webmancer_angular'  },
+    { nome: 'NestJS CRUD ', descricao: 'Backend study for personal project and portfolio', img: 'assets/nst.png', tag: 'Technology, NextJS, NodeJS, Backend, API, PostgreSQL', link: 'https://github.com/caiodcc/backend_webmancer_nestjs' },
   ]// ... mais projetos
 
 }
